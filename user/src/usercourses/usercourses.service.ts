a57c5baccf6fef9dc73e823f43c88a3f5924ddbb
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-01-30 17:19:02 -0300

    create constructor

--- user/src/usercourses/usercourses.service.ts
+++ user/src/usercourses/usercourses.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import {UserCourseEntity} from './entity/userCourse.entity'
 
 @Injectable()
-export class UsercoursesService {}
+export class UsercoursesService {
+  constructor(
+    @InjectRepository(UserCourseEntity)
+    private userCourseRepository: Repository<UserCourseEntity>
+  )
+  {}
+}
